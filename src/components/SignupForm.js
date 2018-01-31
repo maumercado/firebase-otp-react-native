a05b7b6c9940194e9a8d22e035affd6b5e937b96
Mauricio Mercado <mau@maumercado.com>
2018-01-30 21:58:49 -0800

    Add functionality to signup form to signup and receive a code

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { FormLabel, FormInput, Button } from "react-native-elements";
+import axios from "axios";
+
+const ROOT_URL = "https://us-central1-one-time-pwd-b2b7a.cloudfunctions.net";
 
 class SignupForm extends Component {
+    state = { phone: "" };
+
+    handleSubmit = async () => {
+        try {
+            await axios.post(`${ROOT_URL}/createUser`, {
+                phone: this.state.phone
+            });
+
+            await axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+                phone: this.state.phone
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     render() {
         return (
             <View>
                 <View style={{ marginBottom: 10 }}>
                     <FormLabel>Enter Phone Number</FormLabel>
-                    <FormInput />
+                    <FormInput
+                        value={this.state.phone}
+                        onChangeText={phone => this.setState({ phone })}
+                    />
                 </View>
-                <Button title="Submit" />
+                <Button onPress={this.handleSubmit} title="Submit" />
             </View>
         );
     }
